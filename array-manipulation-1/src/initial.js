01d125c4c1237b9017d57fc20461a6220c9b1109
andrewkyungwonpark <andrewkpark@yahoo.com>
2020-08-31 14:34:17 -0700

    Completed array-manipulation-1

---
+++ array-manipulation-1/src/initial.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+function initial(array) {
+  const initialArr = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    initialArr.push(array[i]);
+  }
+  return initialArr;
+}
