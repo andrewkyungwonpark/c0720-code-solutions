796ebac242296f1380dccb0ea10f76b04a4cabe9
andrewkyungwonpark <andrewkpark@yahoo.com>
2020-09-07 09:45:21 -0700

    Updated difference and flatten

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,19 +1,6 @@
 /* eslint-disable no-unused-vars */
 function difference(first, second) {
-  const output = [];
-  for (let i = 0; i < first.length; i++) {
-    if (first[i]) {
-      if (!second.includes(first[i])) {
-        output.push(first[i]);
-      }
-    }
-  }
-  for (let j = 0; j < second.length; j++) {
-    if (second[j]) {
-      if (!first.includes(second[j])) {
-        output.push(second[j]);
-      }
-    }
-  }
-  return output;
+  const firstDiff = first.filter(element => !second.includes(element));
+  const secondDiff = second.filter(element => !first.includes(element));
+  return firstDiff.concat(secondDiff);
 }
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-unused-vars */
 function flatten(array) {
   const output = [];
-  if (array.length === 1) {
-    return [];
-  }
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
       for (let j = 0; j < array[i].length; j++) {
